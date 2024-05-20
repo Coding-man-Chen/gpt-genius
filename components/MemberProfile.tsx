538775f6af1129215b775ca88b43157ac62c9374
Coding-man-Chen <729178954@qq.com>
2024-05-20 15:14:43 +0200

    fix lint bug

--- components/MemberProfile.tsx
+++ components/MemberProfile.tsx
@@ -5,7 +5,7 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 const MemberProfile = async () => {
   const user = await currentUser();
   const {userId} = auth()
-  await fetchOrGenerateTokens(userId)
+  await fetchOrGenerateTokens(userId!)
   return (
     <div className="flex items-center px-4 gap-2">
       <UserButton />
